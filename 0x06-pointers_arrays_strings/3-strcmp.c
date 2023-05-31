060087855a8c808cb53e4db1d7df13ff1f594ca5
Eric Acquah <ericacquah1720@gmail.com>
2023-05-31 22:23:23 +0000

    Resolving string compare function issues

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,8 @@
 
 /**
  *_strcmp - compare two strings
- *@str1: Points to the first string
- *@str2: Points to the second string
+ *@s1: Points to the first string
+ *@s2: Points to the second string
  *Return: (-1) if str1 < str2; (1) if str1 > str2 (0) if both match
  */
 
@@ -15,11 +15,11 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*(s1 + i) < *(s2 + i))
 		{
-			return (-15);
+			return (-1);
 		}
 		else if (*(s1 + i) > *(s2 + i))
 		{
-			return (15);
+			return (1);
 		}
 	}
 
