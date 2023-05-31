a0db707b7825f16a3dee097129d3a05c68ec47b8
Eric Acquah <ericacquah1720@gmail.com>
2023-05-31 21:50:25 +0000

    Compare two strings and return value of match or mismatch

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strcmp - compare two strings
+ *@str1: Points to the first string
+ *@str2: Points to the second string
+ *Return: (-1) if str1 < str2; (1) if str1 > str2 (0) if both match
+ */
+
+int _strcmp(char *str1, char *str2)
+{
+	int i;
+
+	for (i = 0; *(str1 + i) != '\0' && *(str2 + i) != '\0'; i++)
+	{
+		if (*(str1 + i) < *(str2 + i))
+		{
+			return (-1);
+		}
+		else if (*(str1 + i) > *(str2 + i))
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
+int _strcmp(char *str1, char *str2);
 
 #endif
