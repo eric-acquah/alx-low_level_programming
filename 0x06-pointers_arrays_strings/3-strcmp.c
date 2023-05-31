817bc83675468bbe94840ce7697b5e9f0959cbb7
Eric Acquah <ericacquah1720@gmail.com>
2023-05-31 21:55:59 +0000

    Compare two strings and return value of match or mismatch

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *str1, char *str2)
 {
 	int i;
 
-	for (i = 0; *(str1 + i) != '\0' && *(str2 + i) != '\0'; i++)
+	for (i = 0; *(str1 + i) && *(str2 + i) != '\0'; i++)
 	{
 		if (*(str1 + i) < *(str2 + i))
 		{
