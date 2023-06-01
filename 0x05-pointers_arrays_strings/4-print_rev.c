04c2434c189e5945ab7a1a928204a753c29cddd9
Eric Acquah <ericacquah1720@gmail.com>
2023-06-01 18:39:40 +0000

    reverse the order of strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,15 +11,19 @@ void print_rev(char *s)
 	int size = 0;
 	int i, j;
 	char *src = s;
+	char tmp;
 
 	for (i = 0; *(src + i) != '\0'; i++)
 	{
 		size++;
 	}
+	size = size - 1;
 
-	for (j = (size - 1); j >= 0; j--)
+	for (j = 0; j < size; j++)
 	{
-		_putchar(*(src + j));
+		tmp = *(src + j);
+		*(src + size) = *(src + j);
+		*(src + size) = tmp;
+		size--;
 	}
-	_putchar('\n');
 }
