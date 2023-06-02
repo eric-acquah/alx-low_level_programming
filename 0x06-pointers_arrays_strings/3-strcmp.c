09c83b97c9e76b354189928a1d0bce2c8f88a997
Eric Acquah <ericacquah1720@gmail.com>
2023-06-02 12:38:24 +0000

    String compare bug fix1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,20 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int diff;
+	int diff_inverse;
 
 	for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
 	{
 		if (*(s1 + i) < *(s2 + i))
 		{
-			return (*(s1 + i) - *(s2 + i));
+			diff = *(s1 + i) - *(s2 + i);
+			return (diff);
 		}
 		else if (*(s1 + i) > *(s2 + i))
 		{
-			return (*(s1 + i) + *(s2 + i));
+			diff_inverse = *(s1 + i) - *(s2 + i);
+			return (diff_inverse);
 		}
 	}
 
