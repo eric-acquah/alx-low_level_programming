74e5dde913545fa91485c7d839f44503188019d1
Eric Acquah <ericacquah1720@gmail.com>
2023-06-02 18:08:25 +0000

    test run5

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,9 @@ char *cap_string(char *str)
 {
 	int i;
 
+	if (*str >= 'a' && *str <= 'z')
+		*str -= 32;
+
 	/*Loop through the entire string*/
 	for (i = 0; *(str + i) != '\0'; i++)
 	{
@@ -22,11 +25,6 @@ char *cap_string(char *str)
 		    *(str + i) == ')' || *(str + i) == '{' ||
 		    *(str + i) == '}')
 		{
-			/*
-			 *Convert all tabs into sapce
-			 *if (*(str + i) == '\t')
-			 *	*(str + i) = ' ';
-			*/
 
 			/*If the following value is lowercase convert to upper*/
 			if (*(str + (i + 1)) >= 'a' && *(str + (i + 1)) <= 'z')
