b9ff1f29c70581f9a22f08fcd20f4927da659720
Eric Acquah <ericacquah1720@gmail.com>
2023-06-06 17:49:55 +0000

    test run on strange compile fail on the checker side

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_strchr - find a match in string
+ *_strchr - find the 1st match of the specified character in a string
  *@s: pointer to string to be searched
- *@c: Character to find for in string
- *Return: Pointer to string pointerd by 's'
+ *@c: Character to find for in the string
+ *Return: Pointer to string pointed by 's'
  */
 
 char *_strchr(char *s, char c)
@@ -12,13 +12,16 @@ char *_strchr(char *s, char c)
 	int i, j;
 	int count = 0;
 
+	/*Get the lenght of the string*/
 	for (j = 0; *(s + j) != '\0'; j++)
 	{
 		count++;
 	}
 
-	for (i = 0; i < (count + 1); i++)
+	/*Loop over string to find a match*/
+	for (i = 0; i <= count; i++)
 	{
+		/*If match is found return the string to the string pointer*/
 		if (*(s + i) == c)
 		{
 			s = &(*(s + i));
@@ -26,5 +29,7 @@ char *_strchr(char *s, char c)
 		}
 
 	}
+
+	/*If no match is found return NULL to the string pointer*/
 	return (NULL);
 }
