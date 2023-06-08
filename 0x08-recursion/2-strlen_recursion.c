b7146dca3a59bcb2238dfb1c8eac43f05e52764b
Eric Acquah <ericacquah1720@gmail.com>
2023-06-08 23:58:24 +0000

    Resolved bug

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,10 +11,12 @@ int _strlen_recursion(char *s)
 	int i = 0;
 	int count = 0;
 
+	/*Base case; return 0 if value is null*/
 	if (*(s + i) == '\0')
 	{
 		return (count);
 	}
 
-	return (_strlen_recursion(*(s + (i + 1)) + count));
+	/*Recursive case; return the function plus count*/
+	return (_strlen_recursion((s + (i + 1)) + count));
 }
