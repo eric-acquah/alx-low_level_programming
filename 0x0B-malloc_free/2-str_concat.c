edec5ee01803b738468887449d23798b579732b9
Eric Acquah <ericacquah1720@gmail.com>
2023-06-14 11:43:23 +0000

    changes to null argument inputs - test10

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,18 +23,21 @@ char *str_concat(char *str1, char *str2)
 	if (strbuf == NULL)
 		return (NULL);
 
-	if (str1 == NULL)
-	{
-		str1 = "";
-	}
-	else if (str2 == NULL)
-		str2 = "";
-
 	for (i = 0; i < len1; i++)
+	{
+		if (str1 == NULL)
+			str1 = "";
+
 		*(strbuf + i) = *(str1 + i);
+	}
 
 	for (j = 0; j < len2; j++)
+	{
+		if (str2 == NULL)
+			str2 = "";
+
 		*(strbuf + (len1 + j)) = *(str2 + j);
+	}
 
 	*(strbuf + (size + 1)) = '\0';
 
