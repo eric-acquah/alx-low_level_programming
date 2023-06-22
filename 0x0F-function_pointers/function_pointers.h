8967d855d3b9d0979e549d1cfd73ab3b1dd74486
Eric Acquah <ericacquah1720@gmail.com>
2023-06-22 15:16:05 +0000

    adding header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
+#include <stddef.h>
+
 #ifndef ANYTHING
 #define ANYTHING
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
