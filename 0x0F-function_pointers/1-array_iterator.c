faa418993479217abf8c182e62cf1a8911724982
Eric Acquah <ericacquah1720@gmail.com>
2023-06-22 13:16:03 +0000

    Execute a function for each array element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ *array_iterator - execute (call-back) a funtion for each array element
+ *@array: array to work with
+ *@size: size of given array
+ *@action: funtion pointer argument
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
