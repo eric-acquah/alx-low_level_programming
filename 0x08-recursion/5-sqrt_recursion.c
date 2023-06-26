c11ad9362761259408c1c9a0b9b4ee037b6847f3
Eric Acquah <ericacquah1720@gmail.com>
2023-06-26 11:59:29 +0000

    find the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,5 @@
 #include "main.h"
 
-/**
- *_sqrt_recursion - return the natural square root of a number
- *@n: number whose square root is to be found
- *Return: natural square root of n
- */
-
-int cycle(int y, int n);
-
-int _sqrt_recursion(int n)
-{
-	int x = 1;
-
-	if ((x * x) == n)
-	{
-		return (x);
-	}
-
-	return (x = cycle(x, n));
-}
-
 
 /**
  *cycle - process the square root of a number
@@ -41,4 +21,22 @@ int cycle(int y, int n)
 	}
 
 	return (-1);
+}
+
+/**
+ *_sqrt_recursion - return the natural square root of a number
+ *@n: number whose square root is to be found
+ *Return: natural square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int x = 1;
+
+	if ((x * x) == n)
+	{
+		return (x);
+	}
+
+	return (x = cycle(x, n));
 }
