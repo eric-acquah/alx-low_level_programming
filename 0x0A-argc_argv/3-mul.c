4bc41a9f7d432b039fa54eb96b405b71d0b900ae
Eric Acquah <ericacquah1720@gmail.com>
2023-06-29 10:22:28 +0000

    perform multiplication on two numbers received from the commandline

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - multiple two numbers passed from the command line
+ *@argc: number of arguments passed
+ *@argv: pointer to argument list
+ *Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	return (0);
+}
