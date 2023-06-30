5633a971b7e71c0206bc3cd325b098d4bbcdb71a
Eric Acquah <ericacquah1720@gmail.com>
2023-06-30 14:34:18 +0000

    Trying another method for printing (nil) if NULL is passed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,27 +14,13 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 	{
 		d->name = "(nil)";
-		printf("Name: %s\n", d->name);
-	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-	}
-	if (d->age != 0)
-	{
-		printf("Age: %f\n", d->age);
-	}
-	else
-	{
-		printf("Age : %f\n", 0.0);
 	}
 	if (d->owner == NULL)
 	{
 		d->owner = "(nil)";
-		printf("Owner: %s\n", d->owner);
 	}
-	else
-	{
-		printf("Owner: %s\n", d->owner);
-	}
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
