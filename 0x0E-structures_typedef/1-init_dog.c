734384bedf79a6fec2be4e4ed7d2bb97095ac05e
Eric Acquah <ericacquah1720@gmail.com>
2023-06-30 00:36:53 +0000

    test run 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,4 @@
-#include "main.h"
-
-/**
- *struct dog - store dog info
- *@name: name of dog
- *@age: age of dog
- *@owner: client name
- */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+#include "dog.h"
 
 
 /**
