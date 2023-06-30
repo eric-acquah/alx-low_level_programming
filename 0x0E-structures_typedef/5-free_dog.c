efd0a31c83bdecfa5a956bd05d0fe7f0cf13739d
Eric Acquah <ericacquah1720@gmail.com>
2023-06-30 16:18:09 +0000

    Free memory allocated to struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *free_dog - free memory space dynamicaly allocated to struct dog onject
+ *@d: pointer to struct dog object
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
