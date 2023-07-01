800daff84665acab0d97bfde8ba19ab818d4bba3
Eric Acquah <ericacquah1720@gmail.com>
2023-07-01 23:08:11 +0000

    Handeled str null pointer error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,17 @@ size_t print_list(list_t *h)
 {
 	size_t counter = 0;
 
-	/*if (h == NULL)
-		return (0);*/
 	while (h != 0)
 	{
 		if (h->str == NULL)
 		{
 			h->str = "[0] (nil)";
+			printf("%s\n", h->str);
 		}
+		else
+		{
 		printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		counter++;
 	}
