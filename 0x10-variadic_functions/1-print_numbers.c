de3df775bd6f0a9b9e4cfecac006afe9be296808
Eric Acquah <ericacquah1720@gmail.com>
2023-07-02 16:28:54 +0000

    Extract and print numbers from argument list

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - print numbers extracted from the argument list
+ *@separator: arguments separator
+ *@n: number of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list figures;
+	unsigned int i, digit;
+
+	va_start(figures, n);
+
+	for (i = 0; i < n; i++)
+	{
+		digit = va_arg(figures, int);
+
+		if (separator == NULL)
+			separator = "\0";
+		if (i < n - 1)
+			printf("%d%s", digit, separator);
+		else
+			printf("%d", digit);
+	}
+	printf("\n");
+	va_end(figures);
+}
