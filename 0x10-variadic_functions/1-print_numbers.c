8d59ca90970b84e5e6ef2244cd5f256bb48522d0
Eric Acquah <ericacquah1720@gmail.com>
2023-07-02 16:34:36 +0000

    Minor fix; adding header file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 
 /**
  *print_numbers - print numbers extracted from the argument list
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,10 @@
 #define VAR_ARG
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
