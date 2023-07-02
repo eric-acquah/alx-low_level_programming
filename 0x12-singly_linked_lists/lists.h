7f71d566f73d99e2c67533d6a1e24a454ffb9e26
Eric Acquah <ericacquah1720@gmail.com>
2023-07-02 02:06:30 +0000

    test run2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,7 +20,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
+size_t print_list(list_t *h);
 
 size_t list_len(const list_t *h);
 
