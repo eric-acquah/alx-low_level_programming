0d7cb8d190cdcb593d3736cdf9394bd0efa25963
Eric Acquah <ericacquah1720@gmail.com>
2023-07-03 09:13:34 +0000

    Print to stdout according to specified format

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+
+/**
+ *print_all - print according to custom format specifier
+ *
+ *@format: format specifier
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list specifier;
+	int idx;
+	char *str;
+	char *delimeter = ", ";
+
+	va_start(specifier, format);
+	idx = 0;
+	while (format[idx] != '\0')
+	{
+		if (idx + 1 > 1)
+			printf("%s", delimeter);
+		switch (format[idx])
+		{
+		case 'i':
+			printf("%d", va_arg(specifier, int));
+			break;
+		case 'c':
+			printf("%c", (char)va_arg(specifier, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(specifier, double));
+			break;
+		case 's':
+			str = va_arg(specifier, char *);
+			if (str != NULL)
+				printf("%s", str);
+			else if (str == NULL)
+				printf("(nil)");
+			break;
+		default:
+			printf("%c", '\0');
+		}
+		idx++;
+	}
+	 printf("\n");
+	va_end(specifier);
+}
