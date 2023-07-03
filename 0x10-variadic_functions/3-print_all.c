b3dc9d544be3195a62c0fae2d949ac1b5db209d4
Eric Acquah <ericacquah1720@gmail.com>
2023-07-03 13:34:27 +0000

    tidied up code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,48 +3,45 @@
  *print_all - print according to custom format specifier
  *@format: format specifier
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list specifier;
-	int idx = 0;
-	char *str, *delimeter = ", ";
+	int idx = 0, sum = 0;
+	char *str, *d = ", ";
 
 	va_start(specifier, format);
 	if (format != NULL)
 	{
 		while (format[idx] != '\0')
 		{
-			if (idx + 1 > 1)
-				printf("%s", delimeter);
 			switch (format[idx])
 			{
 			case 'i':
-				printf("%d", va_arg(specifier, int));
+				printf("%d%s", va_arg(specifier, int), d);
 				break;
 			case 'c':
-				printf("%c", (char)va_arg(specifier, int));
+				printf("%c%s", (char)va_arg(specifier, int), d);
 				break;
 			case 'f':
-				printf("%f", va_arg(specifier, double));
+				printf("%f%s", va_arg(specifier, double), d);
 				break;
 			case 's':
 				str = va_arg(specifier, char *);
 				switch (str == NULL)
 				{
 				case 1:
-					printf("(nil)");
+					printf("(nil)%s", d);
 					break;
 				case 0:
-					printf("%s", str);
+					printf("%s%s", str, d);
 				}
 				break;
-			default:
-				printf("%c", '\0');
 			}
-			idx++;
+			sum += idx++;
+			if (idx < sum - 1)/*add ", " excluding the last value to print*/
+				d = "\0";
 		}
 	}
-		printf("\n");
-		va_end(specifier);
+	printf("\n");
+	va_end(specifier);
 }
