894eb152f0d8ee8b022aedbd61c5f04d283da1a5
Eric Acquah <ericacquah1720@gmail.com>
2023-07-03 18:17:49 +0000

    my very first assembly code to print text to stdout

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -9,14 +9,14 @@
 
 	global main		;main = label that marks entery point of progam
 main:
-	sub rsp, 8		;allocate 8bytes to stack pointer
+	sub rsp, 16		;allocate 8bytes to stack pointer
 
 	mov rdi, specifier	;loading specifier as arg to printf
 	mov rsi, str		;loading str as the other argument to printf
 	xor rax, rax		;clears 'rax' register to 0
 	call printf		;make a call to printf
 
-	add rsp, 8		;dellocate the 8bytes allocated before
+	add rsp, 16		;dellocate the 8bytes allocated before
 
 	mov eax, 0x60		;perform a graceful exit of the program
 	xor edi, edi
