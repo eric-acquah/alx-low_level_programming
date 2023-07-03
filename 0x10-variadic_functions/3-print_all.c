2c0ba55cd2cf0cffc7fe6f438cc0ccde64d865d0
Eric Acquah <ericacquah1720@gmail.com>
2023-07-03 10:57:42 +0000

    null case troubleshooting3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,6 @@
 #include "variadic_functions.h"
-
 /**
  *print_all - print according to custom format specifier
- *
  *@format: format specifier
  */
 
@@ -13,42 +11,40 @@ void print_all(const char * const format, ...)
 	char *str, *delimeter = ", ";
 
 	va_start(specifier, format);
-	if (format == NULL)
+	if (format != NULL)
 	{
-		printf("%c", '\0');
-		return;
-	}
-	while (format[idx] != '\0')
-	{
-		if (idx + 1 > 1)
-			printf("%s", delimeter);
-		switch (format[idx])
+		while (format[idx] != '\0')
 		{
-		case 'i':
-			printf("%d", va_arg(specifier, int));
-			break;
-		case 'c':
-			printf("%c", (char)va_arg(specifier, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(specifier, double));
-			break;
-		case 's':
-			str = va_arg(specifier, char *);
-			switch (str == NULL)
+			if (idx + 1 > 1)
+				printf("%s", delimeter);
+			switch (format[idx])
 			{
-			case 1:
-				printf("(nil)");
+			case 'i':
+				printf("%d", va_arg(specifier, int));
 				break;
-			case 0:
-				printf("%s", str);
+			case 'c':
+				printf("%c", (char)va_arg(specifier, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(specifier, double));
+				break;
+			case 's':
+				str = va_arg(specifier, char *);
+				switch (str == NULL)
+				{
+				case 1:
+					printf("(nil)");
+					break;
+				case 0:
+					printf("%s", str);
+				}
+				break;
+			default:
+				printf("%c", '\0');
 			}
-			break;
-		default:
-			printf("%c", '\0');
+			idx++;
 		}
-		idx++;
+		printf("\n");
+		va_end(specifier);
 	}
-	printf("\n");
-	va_end(specifier);
 }
