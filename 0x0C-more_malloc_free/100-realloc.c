dd1533e78e25fc07afe1fe6b6a8d7810fa8d0fe5
Eric Acquah <ericacquah1720@gmail.com>
2023-07-05 21:00:07 +0000

    Fixed memory leak problem for good

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,15 +31,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
+		free(ptr);
 		ptr = resize;
+	}
+	else if (ptr != NULL)
+	{
+		new_clone = (char *)resize;
+		old_clone = (char *)ptr;
+
+		for (j = 0; j < new_size && j < old_size; j++)
+			new_clone[j] = old_clone[j];
+
 		free(ptr);
 	}
-
-	new_clone = (char *)resize;
-	old_clone = (char *)ptr;
-
-	for (j = 0; j < new_size && j < old_size; j++)
-		new_clone[j] = old_clone[j];
-
-	return (resize);
+		return (resize);
 }
