93badf572774d6d40af54cd47eac682892551284
Eric Acquah <ericacquah1720@gmail.com>
2023-07-05 20:18:56 +0000

    Fixed memory leak

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,7 +30,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	if (ptr == NULL)
+	{
+		free(ptr);
 		ptr = resize;
+	}
 
 	new_clone = (char *)resize;
 	old_clone = (char *)ptr;
