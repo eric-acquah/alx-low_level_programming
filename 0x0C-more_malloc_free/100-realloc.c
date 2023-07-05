324eb0ed6219e729d381fc74208458eca11e7181
Eric Acquah <ericacquah1720@gmail.com>
2023-07-05 19:53:11 +0000

    Ensuring proper flow of function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *old_clone;
 	char *new_clone;
 
-	if (new_size == 0 && ptr == NULL)
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
