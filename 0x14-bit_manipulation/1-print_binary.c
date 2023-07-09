528da53332c1f9de4c7adf74f4481ef62232353e
Eric Acquah <ericacquah1720@gmail.com>
2023-07-09 21:46:13 +0000

    print binary representation of a decimal number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ *print_binary - convert decimal numbers to binary and print the value
+ *
+ *@n: binary number argument
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, bit;
+	unsigned long int bimask;
+	int bitsize = sizeof(unsigned long int) * 8;
+	int firstbit = 1;
+
+	if (n == 0)
+		_putchar(n + '0');
+
+	/*this shift the significant bit set in 1 to the left=>(100000000)*/
+	bimask = 1UL << (bitsize - 1);/*UL(unsigned long) it ensure*/
+	/*correct binary value*/
+
+	for (i = 0; i < bitsize; i++)
+	{
+		bit = (n & bimask);/*Operation to extract significant bits in*/
+		if (bit)
+			bit = 1;
+		else
+			bit = 0;
+
+		if (firstbit && bit)/*This how we get rid of leading zeros*/
+		{
+			firstbit = 0;
+			_putchar(bit + '0');
+		}
+		else if (firstbit == 0)
+		{
+			_putchar(bit + '0');
+		}
+
+		/*Shifts to next bit in number to work on*/
+		bimask = bimask >> 1;
+
+	}
+	_putchar('\n');
+
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,7 @@ int _putchar(char c);
 
 unsigned int binary_to_uint(char *b);
 
+void print_binary(long unsigned int n);
+
 
 #endif
