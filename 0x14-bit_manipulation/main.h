6aa1fbaa359fec4303024514bfd3113a3350814c
Eric Acquah <ericacquah1720@gmail.com>
2023-07-29 18:55:54 +0000

    clear bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,5 +12,7 @@ int get_bit(unsigned long int n, unsigned int index);
 
 int set_bit(unsigned long int *n, unsigned int index);
 
+int clear_bit(unsigned long int *n, unsigned int index);
+
 
 #endif
