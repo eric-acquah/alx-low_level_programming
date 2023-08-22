fb6d3e059798757324eefcca136ecc6c8a47203a
Eric Acquah <ericacquah1720@gmail.com>
2023-08-22 01:42:04 +0000

    add node at a specific index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /**
  *insert_dnodeint_at_index - insert a node at a given index
  *@h: pointer to to head node pointer
@@ -14,28 +13,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *newnode, *tmp = *h;
 
-	if (h == NULL)
+	if (h == NULL) /*check if list does not exist*/
 		return (NULL);
-	else if (idx >= list_len(*h) || idx < i)
+	else if (idx >= list_len(*h) || idx < i)/*checking for invalid index*/
 		return (NULL);
+	else if (idx == 0)
+		newnode = add_dnodeint(h, n);/*if index is 0, insert at beginnig*/
 
-	while (i < (idx - 1))
+	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));/*create the newnode*/
+	if (newnode == NULL)
+		return (NULL);/*check if malloc failed*/
+
+	while (i < (idx - 1))/*find the specified index*/
 	{
 		tmp = tmp->next;
 		i++;
 	}
 
-	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
-		return (NULL);
+	if (tmp->next == NULL)/*if index is last index insert at the end*/
+		newnode = add_dnodeint_end(h, n);
 
+	/*else insert at the given position*/
 	newnode->n = n;
 	newnode->next = tmp->next;
 	newnode->prev = tmp;
-	if (newnode->next != NULL)
-		newnode->next->prev = newnode;
 	tmp->next = newnode;
-
+	newnode->next->prev = newnode;
 	return (newnode);
 }
 
